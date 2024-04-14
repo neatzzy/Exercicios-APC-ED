9b2e29cb02cae93acd660282fb3600f42180f0a4
Élvis Corrêa Miranda Júnior <138946211+neatzzy@users.noreply.github.com>
2024-04-14 14:23:09 -0300

    Update 1065 - Pares entre cinco numeros.c

--- 1065 - Pares entre cinco numeros.c
+++ 1065 - Pares entre cinco numeros.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int i, n, pares = 0;
+    int i, nro, pares = 0;
 
-    for(i = 0; i <= 5; i++){
-        scanf("%d", &n);
-        if(n % 2 == 0){
+    for(i = 1; i <= 5; i++){
+        scanf("%d", &nro);
+        if(nro % 2 == 0){
             pares++;
         }
     }
-
     printf("%d valores pares\n", pares);
 
     return 0;
