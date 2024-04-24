535a13f8a5636cc8c010709599d26eed62d22a8a
Élvis Corrêa Miranda Júnior <138946211+neatzzy@users.noreply.github.com>
2024-04-24 19:04:16 -0300

    Create 1101 - Sequencia de numeros e soma.c

---
+++ 1101 - Sequencia de numeros e soma.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+
+    int m, n, idx;
+    int soma = 0;
+
+    scanf("%d %d",&m, &n);
+    
+    while((m > 0) && (n > 0)){
+        if (m > n){
+        for(idx = n ; idx <= m ; idx++){
+            printf("%d ", idx);
+            soma += idx;
+        }
+    }
+    else{
+        for(idx = m ; idx <= n ; idx++){
+            printf("%d ", idx);
+            soma += idx;
+        }
+    }
+    printf("Sum=%d\n", soma);
+    soma = 0;
+    scanf("%d %d",&m, &n);
+    }
+    return 0;
+}
