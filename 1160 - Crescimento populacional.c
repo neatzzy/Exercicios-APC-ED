4f460aaf824684881eb89aa0693ba3e23e5b1ebf
Élvis Corrêa Miranda Júnior <138946211+neatzzy@users.noreply.github.com>
2024-06-30 10:08:17 -0300

    Update 1160 - Crescimento populacional.c

--- 1160 - Crescimento populacional.c
+++ 1160 - Crescimento populacional.c
@@ -2,9 +2,11 @@
 
 int crescimento(long int pa, long int pb, double ga, double gb){
     int anos = 0;
+
     while(pa <= pb && anos < 101){
         pa += (int)((pa * ga) / 100);
         pb += (int)((pb * gb) / 100);
+        
         anos ++;
     }
     return anos;
@@ -19,7 +21,9 @@ int main(){
     
     for(i = 0 ; i < qtd ; i++){
         scanf("%ld %ld %lf %lf", &a, &b, &g1, &g2);
+
         tempo = crescimento(a, b, g1, g2);
+
         if(tempo <= 100){
             printf("%d anos.\n", tempo);
         }
